e2a741ee2fa803205ef434ea7116f141dca5639a
Ngovanloc123 <62205ngovanloc@gmail.com>
2024-03-26 12:34:23 +0700

    Complete

--- Matrix.c
+++ Matrix.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
 
 #define MAX_SIZE 100
 
@@ -56,6 +58,22 @@ List addNodetoList(int n) {
     return headerNode;
 }
 
+List addNodetoList2(int n) {
+    List headerNode = createList();
+    Position p = headerNode;
+    for(int i = 1; i <= n + 1; i++){
+        List newNode = createNode(0);
+        newNode->next = p->next;
+        newNode->prev = p;
+        p->next = newNode;
+        if(newNode->next != NULL){
+            newNode->next->prev = newNode;
+        }
+        p = p->next;
+    }
+    return headerNode;
+}
+
 void addListtoMatrix(Matrix *A, int n) {
     for(int i = 1; i <= n; i++) {
         List headedNode = createList();
@@ -65,14 +83,29 @@ void addListtoMatrix(Matrix *A, int n) {
 }
 
 void deleteNodeMiddle(Matrix *A, int n) {
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++) {
         Position p = (*A)->PointToHeadedNode[i];
-        for(int j = 1; j <= n / 2 + 1; j++){
+        for(int j = 1; j <= (n + 1) / 2 + 1; j++){
             p = p->next;
         }
         p->prev->next = p->next;
         p->next->prev = p->prev;
     }
+}
+
+List getNodeFromMarix(Matrix A, int x, int y) {
+    Position p = A->PointToHeadedNode[x];
+    for(int i = 1; i <= y; i++){
+        p = p->next;
+    }
+    return p;
+}
+
+List getNodeFromList(List headedNode, int x) {
+    for(int i = 1; i <= x; i++){
+        headedNode = headedNode->next;
+    }
+    return headedNode;
 }
 
 void displayMatrix(Matrix A, int n) {  
@@ -86,18 +119,54 @@ void displayMatrix(Matrix A, int n) {
     }
 }
 
+void displayList(List headedNode){
+    headedNode = headedNode->next;
+    while(headedNode != NULL) {
+        printf("%.3f\t", headedNode->data);
+        headedNode = headedNode->next;
+    }
+}
+
+void charge(int n, Matrix A, List HeadedNodeB, List N0, float err) {
+    List N1 = addNodetoList2(n - 1);
+    bool dk;
+    do{
+        dk = 0;
+        for(int i = 1; i <= n; i++) {
+            float s = 0;
+            for(int j = 1; j <= n; j++)
+                if(i != j) s += getNodeFromMarix(A, i, j)->data * getNodeFromList(N0,j)->data;
+            getNodeFromList(N1, i)->data = (getNodeFromList(HeadedNodeB, i)->data - s) / getNodeFromMarix(A, i, i)->data;
+            if(fabs(getNodeFromList(N1, i)->data - getNodeFromList(N0, i)->data) >= err) dk = 1;
+        }
+        for(int i = 1; i <= n; i++) getNodeFromList(N0, i)->data = getNodeFromList(N1, i)->data;
+    }while (dk);
+}
 
 int main() {
     Matrix A = createMatrix(MAX_SIZE);
 
-    int n; scanf("%d", &n);
+    int n; printf("Nhap so an:\n"); scanf("%d", &n);
 
+    printf("Nhap Ma Tran:\n");
     addListtoMatrix(&A, n);
-    
     displayMatrix(A, n);
 
     deleteNodeMiddle(&A, n);
     printf("AfterDelete!!!\n");
     displayMatrix(A, n);
+
+    printf("Nhap List B:\n");
+    List headedNodeB = addNodetoList(n - 1);
+
+    printf("Nhap nghiem ban dau:\n");
+    List N0 = addNodetoList(n - 1);
+
+    float err; printf("Nhap sai so:\n"); scanf("%f", &err);
+    charge(n, A, headedNodeB, N0, err);
+
+    printf("Nghiem he phuong trinh:\n");
+    displayList(N0);
+
     return 0;
 }
